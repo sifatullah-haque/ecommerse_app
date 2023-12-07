d6cab11392762866ddfd296d6a1b21034a6f336a
sifatullah-haque <sifatulla.haque@gmail.com>
2023-12-07 22:50:15 +0600

    secondary cart and page created

---
+++ ecomapp/lib/pages/secondary_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("Cart Page"),
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+      ),
+    );
+  }
+}
